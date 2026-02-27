97743a66acc7a1beadc8bedb55185a9a56dae474
DangDuongPH50874 <162089881+DangDuongPH50874@users.noreply.github.com>
2026-02-27 21:03:41 +0700

    CRUD Promotion

--- src/main/java/com/sd61/urbankicks/features/promotion/repository/VoucherRepository.java
+++ src/main/java/com/sd61/urbankicks/features/promotion/repository/VoucherRepository.java
@@ -1,4 +1,4 @@
- package com.sd61.urbankicks.repositories;
+ package com.sd61.urbankicks.features.promotion.repository;
 
 import com.sd61.urbankicks.entity.Voucher;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface VoucherRepository extends JpaRepository<Voucher, UUID> {
+    boolean existsByCode(String code);
 }
